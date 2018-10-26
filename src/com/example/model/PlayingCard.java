3eeda6f5237a9096b665ba685a85706c9e105c73
ritukothari <ritukothari2@gmail.com>
2018-10-26 10:20:29 -0400

    Removed extra prints

--- src/com/example/model/PlayingCard.java
+++ src/com/example/model/PlayingCard.java
@@ -3,10 +3,7 @@ package com.example.model;
 public class PlayingCard {
     public static void main(String[] args) {
         Deck playingDeck = new Deck();
-        System.out.println("******************");
-        System.out.println("Shuffled Card");
         playingDeck.shuffle(playingDeck.getDeck());
-        System.out.println("******************");
         playingDeck.getDeck().forEach(card -> System.out.println(card.toString()));
         System.out.println("******************");
         for(int i=0; i<=52;i++){
